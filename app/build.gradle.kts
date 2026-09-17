5d431e17bb4ad48567b23a3a9f9ff542c816b499
Raqhib <raqhib.ataillah@gmail.com>
2026-09-17 14:40:09 +0800

    feat(version) : up version 5 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.co.mondo.tictactoe"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
